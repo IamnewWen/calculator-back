0a43ccc6f393ef2bce8f75e0c08f8b4c64b156b2
Wen <bert60805@gmail.com>
2024-06-02 17:21:53 +0800

    feat: formula operation

--- routes/calculatorRouter.js
+++ routes/calculatorRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { add, subtract, multiply, divide } = require('../service/operate')
+const { add, subtract, multiply, divide, formula } = require('../service/operate')
 
 router.get('/', (req, res) => {
   res.status(200).send('This is a calculator')
@@ -10,8 +10,9 @@ router.get('/add', (req, res) => {
   const QUERY = req.query
   const v1 = Number(QUERY.v1)
   const v2 = Number(QUERY.v2)
+  const answer = add(v1, v2) // 不要直接把 answer放進 json，放在前一層有需要的話可以添加檢查功能
   res.json({
-    answer: add(v1, v2)
+    answer: answer
   })
 })
 
@@ -19,8 +20,9 @@ router.get('/subtract', (req, res) => {
   const QUERY = req.query
   const v1 = Number(QUERY.v1)
   const v2 = Number(QUERY.v2)
+  const answer = subtract(v1, v2)
   res.json({
-    answer: subtract(v1, v2)
+    answer: answer
   })
 })
 
@@ -28,8 +30,9 @@ router.get('/multiply', (req, res) => {
   const QUERY = req.query
   const v1 = Number(QUERY.v1)
   const v2 = Number(QUERY.v2)
+  const answer = multiply(v1, v2)
   res.json({
-    answer: multiply(v1, v2)
+    answer: answer
   })
 })
 
@@ -37,8 +40,21 @@ router.get('/divide', (req, res) => {
   const QUERY = req.query
   const v1 = Number(QUERY.v1)
   const v2 = Number(QUERY.v2)
+  const answer = divide(v1, v2)
   res.json({
-    answer: divide(v1, v2)
+    answer: answer
+  })
+})
+
+router.get('/formula', (req, res) => {
+  const QUERY = req.query
+  const value = QUERY.value
+
+  console.log(value)
+
+  const answer = formula(value)
+  res.json({
+    answer: answer
   })
 })
 
--- service/operate.js
+++ service/operate.js
@@ -4,3 +4,62 @@ exports.add = (v1, v2) => v1 + v2
 exports.subtract = (v1, v2) => v1 - v2
 exports.multiply = (v1, v2) => v1 * v2
 exports.divide = (v1, v2) => Number((v1 / v2).toFixed(ROUND))
+exports.formula = (value) => {
+  formattedFormula = test(value)
+  result = formulaOperator(formattedFormula)
+
+  return result
+}
+
+// const formulaFormatter = (formula) => {
+//   formattedFormula = formula.replace(/\s/g, '').split('') //remove all whitespace using Regex, and split into array
+//   return formattedFormula
+// }
+
+const formulaOperator = (formattedFormula) => {
+  return formattedFormula
+}
+
+const test = (formula) => {
+  let result = []
+  let addPart = []
+  let subtractPart = []
+
+  formattedFormula = formula.replace(/\s/g, '')
+  if (formattedFormula.includes('+')) {
+    result = formattedFormula.split('+')
+  }
+
+  result.forEach((element, index) => {
+    if (element.includes('-')) {
+      let part = element.split('-')
+      part.forEach((element, index) => {
+        if (index === 0) {
+          addPart.push(element)
+        }
+        subtractPart.push(element)
+      })
+    } else {
+      addPart.push(element)
+    }
+  })
+
+  console.log(addPart)
+  console.log(subtractPart)
+
+  multiplyAndDivide(subtractPart)
+
+  return result
+}
+
+const multiplyAndDivide = (part) => {
+  let result = []
+  let op = []
+
+  part.forEach((element) => {
+    if (element.includes('*')) {
+      op = element.split('*')
+    }
+    console.log(op)
+  })
+}
